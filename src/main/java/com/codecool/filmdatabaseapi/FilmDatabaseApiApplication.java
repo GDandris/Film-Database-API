d62cd5169a41060683ce50692c6bd89bbd6d69fc
GDandris <andris.gede@gmail.com>
2021-08-04 11:55:44 +0200

    Initialize Spring project

---
+++ src/main/java/com/codecool/filmdatabaseapi/FilmDatabaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.filmdatabaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmDatabaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmDatabaseApiApplication.class, args);
+	}
+
+}
