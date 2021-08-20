c08b3500acfb0fe9702cae77b6fbfca682fcf0f2
GDandris <andris.gede@gmail.com>
2021-08-20 18:52:47 +0200

    add DirectorIntegrationTest class

---
+++ src/test/java/com/codecool/filmdatabaseapi/integration/DirectorIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.codecool.filmdatabaseapi.integration;
+
+import com.codecool.filmdatabaseapi.model.Director;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+public class DirectorIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private String baseUrl;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @BeforeEach
+    public void setUp() {
+        this.baseUrl = "http://localhost:" + port + "/director";
+    }
+}
