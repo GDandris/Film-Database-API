b97e1f97deb00410f4c83bb34c4f23808dc33534
GDandris <andris.gede@gmail.com>
2021-08-20 16:48:33 +0200

    implement getFilms_notEmptyDatabse test

--- src/test/java/com/codecool/filmdatabaseapi/integration/FilmIntegrationTests.java
+++ src/test/java/com/codecool/filmdatabaseapi/integration/FilmIntegrationTests.java
@@ -11,6 +11,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,13 +32,13 @@ public class FilmIntegrationTests {
     @BeforeEach
     public void setUp() {
         this.baseUrl = "http://localhost:" + port + "/film";
+        Director director = new Director(1L, "Stanley Kubrick");
+        testRestTemplate.postForObject("http://localhost:" + port + "/director", director, Director.class);
     }
 
     @Test
     public void addNewFilm_emptyDatabase_shouldReturnSameFilm() {
-        Film testFilm = new Film();
-        testFilm.setName("Full Metal Jacket");
-        testFilm.setReleaseYear(1987);
+        Film testFilm = new Film(null, "Full Metal Jacket", 1987, null);
         Director director = new Director();
         director.setId(1L);
         director.setName("Stanley Kubrick");
@@ -52,6 +53,23 @@ public class FilmIntegrationTests {
     public void getFilms_emptyDatabase_returnsEmptyList() {
         List<Film> filmList = List.of(testRestTemplate.getForObject(baseUrl, Film[].class));
         assertEquals(0, filmList.size());
+    }
+
+    @Test
+    public void getFilms_notEmptyDatabase_shouldReturnFilmList(){
+        List<Film> testFilmList = new ArrayList<>();
+        testFilmList.add(new Film (null, "Full Metal Jacket", 1987, new Director(1L, "Stanley Kubrick")));
+        testFilmList.add(new Film (null, "The Shining", 1980, new Director(1L, "Stanley Kubrick")));
+        for (Film film : testFilmList) {
+            testRestTemplate.postForObject(baseUrl, film, Film.class);
+        }
+        List<Film> filmList = List.of(testRestTemplate.getForObject(baseUrl, Film[].class));
+        assertEquals(testFilmList.size(), filmList.size());
+        for( int i = 0; i < testFilmList.size(); i++) {
+            assertEquals(testFilmList.get(i).getName(), filmList.get(i).getName());
+            assertEquals(testFilmList.get(i).getReleaseYear(), filmList.get(i).getReleaseYear());
+            assertEquals(testFilmList.get(i).getDirector().getName(), filmList.get(i).getDirector().getName());
+        }
     }
 
 }
