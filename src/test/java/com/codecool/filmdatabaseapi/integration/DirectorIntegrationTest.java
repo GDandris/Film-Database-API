1cbdc0aff25ab65f8ffc9071f14031c6d3490959
GDandris <andris.gede@gmail.com>
2021-08-20 18:58:36 +0200

    implement addNewDirector test

--- src/test/java/com/codecool/filmdatabaseapi/integration/DirectorIntegrationTest.java
+++ src/test/java/com/codecool/filmdatabaseapi/integration/DirectorIntegrationTest.java
@@ -1,13 +1,19 @@
 package com.codecool.filmdatabaseapi.integration;
 
 import com.codecool.filmdatabaseapi.model.Director;
+import com.codecool.filmdatabaseapi.model.Film;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -26,4 +32,12 @@ public class DirectorIntegrationTest {
     public void setUp() {
         this.baseUrl = "http://localhost:" + port + "/director";
     }
+
+    @Test
+    public void addNewDirector_emptyDatabase_shouldReturnSameDirector() {
+        Director testDirector = new Director(null, "Stanley Kubrick");
+        Director result = testRestTemplate.postForObject(baseUrl, testDirector, Director.class);
+        assertEquals(testDirector.getName(), result.getName());
+    }
+
 }
