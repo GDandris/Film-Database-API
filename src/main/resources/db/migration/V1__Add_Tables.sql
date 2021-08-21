28c8787316642d8b2a748d114e18b039d7a4c156
GDandris <andris.gede@gmail.com>
2021-08-21 13:24:30 +0200

    fix flyway V1

--- src/main/resources/db/migration/V1__Add_Tables.sql
+++ src/main/resources/db/migration/V1__Add_Tables.sql
@@ -1,3 +1,8 @@
+drop table if exists FILM CASCADE;
+drop table if exists DIRECTOR CASCADE;
+drop sequence if exists hibernate_sequence;
+create sequence hibernate_sequence start with 1 increment by 1;
+
 create table FILM
 (
                         ID long identity NOT NULL PRIMARY KEY,
@@ -12,3 +17,5 @@ create table DIRECTOR
                         ID long identity NOT NULL PRIMARY KEY,
                         NAME varchar
 );
+
+alter table film add constraint FK4pqpakiyka8wwbiicmc0rgtwi foreign key (director_id) references DIRECTOR;
