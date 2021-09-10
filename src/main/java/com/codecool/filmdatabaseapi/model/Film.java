f014c2b0647e2ea652c8edf1ba4f5b15be9077fe
GDandris <andris.gede@gmail.com>
2021-09-10 13:25:59 +0200

    add Tag entity

--- src/main/java/com/codecool/filmdatabaseapi/model/Film.java
+++ src/main/java/com/codecool/filmdatabaseapi/model/Film.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,5 +22,14 @@ public class Film {
     private int releaseYear;
     @ManyToOne
     private Director director;
+    @ManyToMany
+    private List<Tag> tags = new ArrayList<>();
+
+    public Film(Long id, String name, int releaseYear, Director director) {
+        this.id = id;
+        this.name = name;
+        this.releaseYear = releaseYear;
+        this.director = director;
+    }
 
 }
