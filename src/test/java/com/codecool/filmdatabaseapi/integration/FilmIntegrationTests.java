fa5c6a0dc22f7f31b6fc64ed83e91624a8a0303d
GDandris <andris.gede@gmail.com>
2021-09-12 11:29:38 +0200

    implement getFilmByTags tests

--- src/test/java/com/codecool/filmdatabaseapi/integration/FilmIntegrationTests.java
+++ src/test/java/com/codecool/filmdatabaseapi/integration/FilmIntegrationTests.java
@@ -35,6 +35,10 @@ public class FilmIntegrationTests {
         this.baseUrl = "http://localhost:" + port + "/film";
         Director director = new Director("Stanley Kubrick");
         testRestTemplate.postForObject("http://localhost:" + port + "/director", director, Director.class);
+        Tag tag1 = new Tag("War");
+        Tag tag2 = new Tag("Black & White");
+        testRestTemplate.postForObject("http://localhost:" + port + "/tag", tag1, Tag.class);
+        testRestTemplate.postForObject("http://localhost:" + port + "/tag", tag2, Tag.class);
     }
 
     @Test
@@ -137,6 +141,59 @@ public class FilmIntegrationTests {
         }
 
 
+    }
+
+    @Test
+    public void getFilmByTags_withOneTagAdded_returnsFilmsWithTag() {
+        Director director = new Director();
+        director.setId(1L);
+        director.setName("Stanley Kubrick");
+        Film film1 = new Film ("Full Metal Jacket", 1987, director);
+        Film film2 = new Film ("The Shining", 1980, director);
+        Film film3 = new Film ("Eyes Wide Shut", 1999, director);
+        Film film4 = new Film("Paths of Glory", 1957, director);
+        List<Tag> tags = new ArrayList<>();
+        Tag tag = new Tag("War");
+        tag.setId(1L);
+        tags.add(tag);
+        film1.setTags(tags);
+        film4.setTags(tags);
+        List<Film> testFilms = new ArrayList<>();
+        testFilms.add(film1);
+        testFilms.add(film2);
+        testFilms.add(film3);
+        testFilms.add(film4);
+
+        testFilms.forEach(film -> film.setId(testRestTemplate.postForObject(baseUrl, film, Film.class).getId()));
+
+        testFilms.remove(film2);
+        testFilms.remove(film3);
+
+        List<Film> result = List.of(testRestTemplate.getForObject(baseUrl+"/tag?tagId=1", Film[].class));
+
+        for(int i = 0; i< testFilms.size(); i++) {
+            assertEquals(testFilms.get(i), result.get(i));
+        }
+
+    }
+
+    @Test
+    public void getFilmByTags_withMultipleTagAdded_returnFilmWithTags() {
+        Director director = new Director();
+        director.setId(1L);
+        director.setName("Stanley Kubrick");
+        Film testFilm = new Film("Paths of Glory", 1957, director);
+        List<Tag> tags = new ArrayList<>();
+        Tag tag1 = new Tag("War");
+        Tag tag2 = new Tag("Black & White");
+        tag1.setId(1L);
+        tag2.setId(2L);
+        tags.add(tag1);
+        tags.add(tag2);
+        testFilm.setTags(tags);
+        testRestTemplate.postForObject(baseUrl, testFilm, Film.class);
+        Film result = testRestTemplate.getForObject(baseUrl+"/tag?tagId=1&tagId=2", Film[].class)[0];
+        assertEquals(testFilm, result);
     }
 
 }
