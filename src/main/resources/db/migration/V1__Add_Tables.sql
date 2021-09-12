10c829beacb5b525fc4465b4e155479b7ee4301b
GDandris <andris.gede@gmail.com>
2021-09-12 16:44:19 +0200

    update migration files

--- src/main/resources/db/migration/V1__Add_Tables.sql
+++ src/main/resources/db/migration/V1__Add_Tables.sql
@@ -1,5 +1,7 @@
 drop table if exists FILM CASCADE;
 drop table if exists DIRECTOR CASCADE;
+drop table if exists film_tags CASCADE
+drop table if exists tag CASCADE
 drop sequence if exists hibernate_sequence;
 create sequence hibernate_sequence start with 1 increment by 1;
 
@@ -17,5 +19,17 @@ create table DIRECTOR
                         ID long identity NOT NULL PRIMARY KEY,
                         NAME varchar
 );
+create table FILM_TAGS
+(
+                        FILM_ID bigint not null,
+                        TAGS_ID bigint not null
+)
+create table TAG
+(
+                        id bigint identity NOT NULL PRIMARY KEY,
+                        name varchar(255)
+)
 
 alter table film add constraint FK4pqpakiyka8wwbiicmc0rgtwi foreign key (director_id) references DIRECTOR;
+alter table film_tags add constraint FKjiqv95gpr7og84un3iaxvigd3 foreign key (tags_id) references TAG;
+alter table film_tags add constraint FKdfkbdk423gcenypv9nf94tjsf foreign key (film_id) references FILM;
--- src/main/resources/db/migration/V2__Fill_Tables.sql
+++ src/main/resources/db/migration/V2__Fill_Tables.sql
@@ -5,3 +5,15 @@ INSERT INTO director VALUES (3,'Akira Kurosawa');
 INSERT INTO film VALUES(1,'2001: A Space Odyssey', 1968, 1);
 INSERT INTO film VALUES(2,'The Shining', 1980, 1);
 INSERT INTO film VALUES(3,'The Departed', 2006, 2);
+INSERT INTO film VALUES(4,'Kagemusha', 1980, 3);
+
+INSERT INTO tag VALUES(1, 'Dark');
+INSERT INTO tag VALUES(2, 'Foreign');
+INSERT INTO tag VALUES(3, 'Written-Directed');
+
+INSERT INTO film_tags VALUES(1, 3);
+INSERT INTO film_tags VALUES(2, 1);
+INSERT INTO film_tags VALUES(2, 3);
+INSERT INTO film_tags VALUES(3, 1);
+INSERT INTO film_tags VALUES(4, 2);
+INSERT INTO film_tags VALUES(4, 3);
